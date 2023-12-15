8199b9095e97b309c780bc5afaf03d7e8586de1d
Aslıgül Kaya <asligul.kaya01@gmail.com>
2023-12-15 14:29:45 +0300

    About sayfası tamamlandı

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
-<template>  
-  <div class="  ">
+<template>
+  <div>
     <router-view />
   </div>
-  <Footer/>
 </template>
 
 <style></style>
 <script>
 import sourceData from "@/data.json";
 import NavbarComp from "@/components/NavbarComp.vue";
-import Footer from "@/components/Footer.vue";
 
 export default {
   data() {
@@ -19,15 +17,6 @@ export default {
   },
   components: {
     NavbarComp,
-    Footer,
   },
 };
 </script>
-<style>
-*{
-padding: 0;
-margin: 0;
-box-sizing: border-box;
-scroll-behavior: smooth;
-}
-</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="half">
+    <div class="w-100">
+      <h4>About Us</h4>
+      <h2>Find out more about us</h2>
+    </div>
+    <p class="paragraph">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+.half {
+  position: relative;
+  width: 100%;
+  color: #023646;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  box-sizing: border-box;
+  letter-spacing: 0.076em;
+  font-family: "Zilla Slab", serif;
+}
+
+.paragraph {
+  font-family: "Raleway", sans-serif !important;
+  font-size: 100%;
+  font-weight: 500;
+  margin-top: 1.5em;
+  line-height: 20px;
+}
+</style>
