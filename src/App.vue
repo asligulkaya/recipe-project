e925ea4e887ce1e04cf5f074119aa1214f20d866
Okan Keskin <okannkeskn@gmail.com>
2023-12-15 04:01:02 +0300

    Detay sayfası hazırlandı

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>  
-  <div class="">
+  <div class="  ">
     <router-view />
   </div>
   <Footer/>
@@ -22,3 +22,11 @@ export default {
   },
 };
 </script>
+<style>
+*{
+padding: 0;
+margin: 0;
+box-sizing: border-box;
+scroll-behavior: smooth;
+}
+</style>
--- src/views/RecipeShow.vue
+++ src/views/RecipeShow.vue
@@ -1,33 +1,62 @@
 <template>
-  <NavbarComp/>
-  <div>
-    <h1>{{ recipe.title }}</h1>
-    <div class="recipe-details">
-      <img :src="`/images/${recipe.image}`" :alt="recipe.title" />
-      <h6>{{ recipe.descripton }}</h6>
-      <br />
-      <h6>Ingredients</h6>
-      <ul>
-        <li v-for="ingredient in recipe.ingredients" :key="ingredient">
-          {{ ingredient }}
-        </li>
-      </ul>
-      <h6>Instructions</h6>
-      <br />
-      <ol>
-        <li v-for="(instruction, index) in recipe.instructions" :key="index">
-          {{ instruction }}
-        </li>
-      </ol>
+  <NavbarComp />
+  <div class="row w-full">
+    <div class="col-9 bg-black px-5">
+      <h1 class="mx-auto text-center">{{ recipe.title }}</h1>
+      <div class="recipe-details d-flex flex-column align-items-center">
+        <img
+          :src="`/images/${recipe.image}`"
+          :alt="recipe.title"
+          width="500"
+          class="align-items-center"
+        />
+        <br />
+        
+        <h3 class="mb-4">{{ recipe.descripton }}</h3>  
+        <div class="leftcile d-flex">
+          <br />
+          <div class="col-6">
+            <h5 class="ingredients">Ingredients</h5>
+            <ul>
+              <li v-for="ingredient in recipe.ingredients" :key="ingredient">
+                {{ ingredient }}
+              </li>
+            </ul>
+          </div>
+          <div class="col-6">
+            <h5>Instructions</h5>
+            <br />
+            <ol class="px-4">
+              <li
+                class="mb-3"
+                v-for="(instruction, index) in recipe.instructions"
+                :key="index"
+              >
+                {{ instruction }}
+              </li>
+            </ol>
+          </div>
+          
+          
+        </div>
+        
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="mx-auto" style="width: 100%">
+        <FoodList style="width: 80%" />
+      </div>
     </div>
   </div>
 </template>
 <script>
 import sourceData from "@/data.json";
 import NavbarComp from "@/components/NavbarComp.vue";
+import FoodList from "@/components/FoodList.vue";
 export default {
   components: {
     NavbarComp,
+    FoodList,
   },
   computed: {
     recipeId() {
@@ -39,3 +68,9 @@ export default {
   },
 };
 </script>
+<style scoped>
+.ingredients{
+  text-align: left !important;
+}
+</style>
+
